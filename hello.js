ebd6a6cc13ef18458abab3031c183b9c52750c67
Nilesh Haldar <nileshhaldar98@Nileshs-MacBook-Air.local>
2025-03-14 15:30:53 +0530

    added

---
+++ hello.js
@@ -0,0 +1,17 @@
+function getPizza(people){
+
+    if(people <= 2){
+        console.log("You should get a small pizza");
+
+        
+    } else if(people <= 4){
+        console.log("You should get a medium pizza");
+
+    }
+    else{
+        console.log("You should get a large pizza");
+    }
+
+}
+getPizza(3);
+
